3543ee25b87d76d2e89a9a10111469a2c10dec20
byronlopez <bjlopezc2010@live.com>
2022-10-29 17:06:12 -0600

    adding the reproduction code

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,6 +1,10 @@
-module.exports = {
+/** @type {import('next').NextConfig} */
+const nextConfig = {
   reactStrictMode: true,
+  swcMinify: true,
   experimental: {
-    transpilePackages: ["ui"],
+    appDir: true,
   },
 };
+
+module.exports = nextConfig;
